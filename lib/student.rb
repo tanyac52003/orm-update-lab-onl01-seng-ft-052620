68f389e251a7990756fda8bfd21abe2e9f72c6f0
Tanya Hodges <tanyac52003@gmail.com>
2020-06-29 00:55:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,7 +57,9 @@ end
   
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
-    DB[:conn].execute(sql, name).map { |row| new_from_db(row) }.first
+
+    result = DB[:conn].execute(sql, name)[0]
+    self.new_from_db(result)
   end 
   
     def update
