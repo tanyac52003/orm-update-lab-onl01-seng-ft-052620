363bfff30e196f008d9d906fd44c99a9b118a670
Tanya Hodges <tanyac52003@gmail.com>
2020-06-29 00:34:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,10 +48,8 @@ end
   end 
   
   def self.new_from_db(row)
-     id = row[0]
-    name = row[1]
-    grade = row[2]
-    self.new(id, name, grade)
+    student_new = self.new(row[0], row[1], row[2])
+    student_new
   end 
   
   def find_by_name(name)
