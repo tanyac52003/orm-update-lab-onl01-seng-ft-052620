b72fe278653ac6496c02cbd74141e89ad44fc382
Tanya Hodges <tanyac52003@gmail.com>
2020-06-29 00:43:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ require_relative "../config/environment.rb"
 
 class Student
   attr_accessor :name, :grade 
-  attr_reader :id  
+  attr_writer :id  
   
   def initialize(name, grade, id = nil)
     @id = id 
