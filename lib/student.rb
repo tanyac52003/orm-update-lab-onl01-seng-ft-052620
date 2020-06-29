7d8da46a25d3dce4d1e1814b1a3104217220c547
Tanya Hodges <tanyac52003@gmail.com>
2020-06-29 00:14:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,68 @@
 require_relative "../config/environment.rb"
 
 class Student
-
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
-
-
+  attr_accessor :name, :grade, :id  
+  
+  def initialize(name, grade, id = nil)
+    @id = id 
+    @name = name 
+    @grade = grade
+  end 
+  
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE students(
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER 
+    )
+    SQL
+    DB[:conn].execute(sql)
+  end 
+  
+  def self.drop_table
+    sql = <<-SQL
+    DROP TABLE students
+    SQL
+    DB[:conn].execute(sql)
+  end 
+  
+  def save 
+    self.id 
+    self.update
+   
+    sql = <<-SQL
+      INSERT INTO students(name, grade)
+      VALUES(?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end 
+  
+  def self.create(name, grade) 
+    student = Student.new(name, grade)
+    student.save
+    student 
+  end 
+  
+  def self.new_from_db(row)
+    new_student = self.new 
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
+  end 
+  
+  def find_by_name(name)
+  
+  end 
+  
+    def update
+      
+    end 
+     
 end
+
+
+ # Remember, you can access your database connection anywhere in this class
+  #  with DB[:conn]
