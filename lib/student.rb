6c319dcddab55416f8fdb86eccd459770ea707cd
Tanya Hodges <tanyac52003@gmail.com>
2020-06-29 00:48:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,6 +63,7 @@ end
     DB[:conn].execute(sql, name, grade).map do |row|
       self.new_from_db(row)
   end.first 
+end 
   
     def update
       sql = "UPDATE students SET name =?, grade =? WHERE id =?"
